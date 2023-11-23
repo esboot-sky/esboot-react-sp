588a712839580b34e343976616785e5ac0e8fcc4
Roc <moonlitusun@gmail.com>
2023-11-23 10:49:44 +0800

    fix: upgrade esboot to 2.9.0

--- src/modules/demo/demo.test.tsx
+++ src/modules/demo/demo.test.tsx
@@ -1,10 +1,12 @@
 import { test, expect } from 'vitest';
 import { render } from '@testing-library/react';
 
-import Home from '@/modules/demo/app';
+// import Home from '@/modules/demo/app';
+import { add } from '@/modules/demo/test';
 
 test('Demo', () => {
-  const { container } = render(<Home />);
+  expect(add(1, 2)).toBe(3);
+  // const { container } = render(<Home />);
 
-  expect(container.querySelector('p')?.textContent).toBe('close');
+  // expect(container.querySelector('p')?.textContent).toBe('close');
 });
