8ba6b0dd25833ec04574a37bb1a04cf517086da5
Roc <moonlitusun@gmail.com>
2024-01-04 09:13:05 +0800

    feat: add template loading

--- globals.d.ts
+++ globals.d.ts
@@ -2,5 +2,4 @@ declare interface Window {
   GLOBAL_CONFIG: {
     COMMON_SERVERS: any;
   };
-  esboot_urlParams: Record<string, string>;
 }
--- src/modules/demo/demo.test.tsx
+++ src/modules/demo/demo.test.tsx
@@ -1,11 +1,10 @@
+import { render } from '@testing-library/react';
 import { test, expect } from 'vitest';
 
-// import Home from '@/modules/demo/app';
-import { add } from '@/modules/demo/test';
+import Home from '@/modules/demo/app';
 
 test('Demo', () => {
-  expect(add(1, 2)).toBe(3);
-  // const { container } = render(<Home />);
+  const { container } = render(<Home />);
 
-  // expect(container.querySelector('p')?.textContent).toBe('close');
+  expect(container.querySelector('p')?.textContent).toBe('close');
 });
