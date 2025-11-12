fb633f246b7655fdb3d7529ab570298a6766dc38
Roc <moonlitusun@gmail.com>
2025-11-12 11:28:59 +0800

    chore: add monitor-performance and create-external-console

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
-var GLOBAL_CONFIG = {};
+window.GLOBAL_CONFIG = {
+  debug: true,
+};
--- src/index.entry.tsx
+++ src/index.entry.tsx
@@ -1,3 +1,5 @@
+import { createExternalConsole } from '@dz-web/esboot-browser';
+import { monitorPerformance } from '@dz-web/esboot-browser-react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
@@ -6,6 +8,8 @@ import router from './router';
 
 import '@/styles/index.scss';
 
+monitorPerformance();
+createExternalConsole();
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <StrictMode>
     <RouterProvider router={router} />
